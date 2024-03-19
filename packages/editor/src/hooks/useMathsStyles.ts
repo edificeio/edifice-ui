1013cb82732c0a37dc882155ec2299423cc013ac
Romu-C <romuald.coulaud@gmail.com>
2024-03-19 17:33:07 +0100

    uncomment

--- packages/editor/src/hooks/useMathsStyles.ts
+++ packages/editor/src/hooks/useMathsStyles.ts
@@ -2,25 +2,25 @@ export const useMathsStyles = () => {
   const loadMathsStyles = () => {
     const katexURL =
       "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css";
-    // const hasKatexLink = false;
-    // const links = document.head.getElementsByTagName("link");
+    let hasKatexLink = false;
+    const links = document.head.getElementsByTagName("link");
 
-    // for (const link of links) {
-    //   if (link.href === katexURL) {
-    //     hasKatexLink = true;
-    //     return;
-    //   }
-    // }
+    for (const link of links) {
+      if (link.href === katexURL) {
+        hasKatexLink = true;
+        return;
+      }
+    }
 
-    // if (!hasKatexLink) {
-    const link = document.createElement("link");
-    link.href = katexURL;
-    link.rel = "stylesheet";
-    link.type = "text/css";
+    if (!hasKatexLink) {
+      const link = document.createElement("link");
+      link.href = katexURL;
+      link.rel = "stylesheet";
+      link.type = "text/css";
 
-    document.links;
-    document.head.appendChild(link);
-    // }
+      document.links;
+      document.head.appendChild(link);
+    }
   };
 
   return {
