6f2794fa3ceaf53c24da0dd4f02740bf6a6d04ee
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2024-05-30 09:05:44 +0200

    feat(icons): add reactions icons

---
+++ packages/react/icons/src/audience/reactions/index.ts
@@ -0,0 +1,8 @@
+export { default as Reaction1Circle } from "./Reaction1Circle";
+export { default as Reaction1 } from "./Reaction1";
+export { default as Reaction2Circle } from "./Reaction2Circle";
+export { default as Reaction2 } from "./Reaction2";
+export { default as Reaction3Circle } from "./Reaction3Circle";
+export { default as Reaction3 } from "./Reaction3";
+export { default as Reaction4Circle } from "./Reaction4Circle";
+export { default as Reaction4 } from "./Reaction4";
