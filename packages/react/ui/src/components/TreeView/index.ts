7fadd49025f92fd54d714840f50be2ed7aac466a
Headan <36904370+Headan@users.noreply.github.com>
2024-07-11 10:45:30 +0200

    feat: #WB2-76, add drag and drop on treeview (#172)

--- packages/react/ui/src/components/TreeView/index.ts
+++ packages/react/ui/src/components/TreeView/index.ts
@@ -1,3 +1,5 @@
-export { default as TreeView } from "./TreeView";
+export * from "./TreeItem";
 export * from "./TreeNode";
 export * from "./TreeView";
+export { default as TreeView } from "./TreeView";
+export * from "./hooks/useTreeView";
