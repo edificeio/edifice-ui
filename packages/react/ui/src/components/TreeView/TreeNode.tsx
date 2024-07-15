15be339abdddadd9d9ae6e3fbbc344bf7476f6df
Clement Creusat <clement.creusat@gmail.com>
2024-07-15 16:05:57 +0200

    refactor(treeview): remove TreeItem

--- packages/react/ui/src/components/TreeView/TreeNode.tsx
+++ packages/react/ui/src/components/TreeView/TreeNode.tsx
@@ -1,11 +1,19 @@
+import { useDroppable } from "@dnd-kit/core";
+import { Folder, RafterDown, RafterRight } from "@edifice-ui/icons";
+import clsx from "clsx";
+import { useId } from "react";
+import { useTranslation } from "react-i18next";
 import { TreeData } from "../../types";
-import { TreeItem } from "./TreeItem";
 
 export interface TreeNodeProps {
   /**
    * Data
    */
   node: TreeData;
+  /**
+   * Show or hide section icon (folder icon)
+   */
+  showIcon?: boolean;
   /**
    * Nodes expanded (opened)
    */
@@ -34,43 +42,144 @@ export interface TreeNodeProps {
 
 export const TreeNode = ({
   node,
+  showIcon,
   selectedNodeId,
   expandedNodes,
   draggedNodeId,
   handleItemClick,
   handleToggleNode,
 }: TreeNodeProps) => {
-  const selected = selectedNodeId === node.id;
   const expanded = expandedNodes.has(node.id);
+  const selected = selectedNodeId === node.id;
   const focused = draggedNodeId === node.id;
 
+  const { t } = useTranslation();
+
+  const { setNodeRef } = useDroppable({
+    id: useId(),
+    data: {
+      id: node.id,
+      name: node.name,
+      isTreeview: true,
+      accepts: ["folder", "resource"],
+    },
+  });
+
+  const handleItemKeyDown = (
+    event: React.KeyboardEvent<HTMLDivElement>,
+    nodeId: string,
+  ) => {
+    if (event.code === "Enter" || event.code === "Space") {
+      event.preventDefault();
+      event.stopPropagation();
+
+      handleItemClick?.(nodeId);
+    }
+  };
+
+  const handleItemToggleKeyDown = (
+    event: React.KeyboardEvent<HTMLDivElement>,
+    nodeId: string,
+  ) => {
+    if (event.code === "Enter" || event.code === "Space") {
+      event.preventDefault();
+      event.stopPropagation();
+
+      handleToggleNode?.(nodeId);
+    }
+  };
+
+  const treeItemClasses = {
+    action: clsx("action-container d-flex align-items-center gap-8 px-2", {
+      "drag-focus": focused,
+    }),
+    arrow: clsx("py-8", {
+      invisible: !Array.isArray(node.children),
+    }),
+    button: clsx(
+      "flex-fill d-flex align-items-center text-truncate gap-8 py-8",
+    ),
+  };
+
+  const iconSize = node.section ? 16 : 12;
+
   return (
-    <TreeItem
+    <li
       key={node.id}
-      nodeId={node.id}
-      label={node.name}
-      section={node.section}
-      showIconSection={node.showIconSection}
-      selected={selected}
-      expanded={expanded}
-      focused={focused}
-      onToggleNode={handleToggleNode}
-      onItemClick={handleItemClick}
+      ref={setNodeRef}
+      id={`treeitem-${node.id}`}
+      role="treeitem"
+      aria-selected={selected && selected}
+      aria-expanded={expanded && expanded}
     >
-      {Array.isArray(node.children)
-        ? node.children.map((item) => (
-            <TreeNode
-              key={item.id}
-              node={item}
-              selectedNodeId={selectedNodeId}
-              draggedNodeId={draggedNodeId}
-              expandedNodes={expandedNodes}
-              focused={focused}
-              handleItemClick={handleItemClick}
-              handleToggleNode={handleToggleNode}
-            />
-          ))
-        : null}
-    </TreeItem>
+      <div>
+        <div className={treeItemClasses.action}>
+          <div
+            className={treeItemClasses.arrow}
+            tabIndex={0}
+            role="button"
+            onClick={() => handleToggleNode?.(node.id)}
+            onKeyDown={(event) => handleItemToggleKeyDown(event, node.id)}
+            aria-label={t("foldUnfold")}
+          >
+            {Array.isArray(node.children) &&
+              !!node.children.length &&
+              (expanded ? (
+                <RafterDown
+                  title={t("foldUnfold")}
+                  width={iconSize}
+                  height={iconSize}
+                />
+              ) : (
+                <RafterRight
+                  title={t("foldUnfold")}
+                  width={iconSize}
+                  height={iconSize}
+                />
+              ))}
+
+            {/* Hide rafter when no children to keep alignment */}
+            {!Array.isArray(node.children) && !node.section && (
+              <RafterRight
+                title={t("foldUnfold")}
+                width={iconSize}
+                height={iconSize}
+                aria-hidden="true"
+              />
+            )}
+          </div>
+          <div
+            tabIndex={0}
+            role="button"
+            className={treeItemClasses.button}
+            onClick={() => handleItemClick?.(node.id)}
+            onKeyDown={(event) => handleItemKeyDown(event, node.id)}
+          >
+            {node.section && showIcon && (
+              <Folder title={t("folder")} width={20} height={20} />
+            )}
+            <span className="text-truncate">{node.name}</span>
+          </div>
+        </div>
+
+        {Array.isArray(node.children) && !!node.children.length && expanded && (
+          <ul role="group">
+            {node.children.map((child) => {
+              return (
+                <TreeNode
+                  key={child.id}
+                  node={child}
+                  selectedNodeId={selectedNodeId}
+                  expandedNodes={expandedNodes}
+                  draggedNodeId={draggedNodeId}
+                  handleItemClick={handleItemClick}
+                  handleToggleNode={handleToggleNode}
+                />
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </li>
   );
 };
