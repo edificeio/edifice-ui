b14f0e08e6d68baa1312dc845501cb1ff6498412
Clement Creusat <clement.creusat@gmail.com>
2024-07-18 10:17:20 +0200

    feat(client): #WB2-1815, export ResourceService and remove WikiResourceService

--- packages/client/src/ts/services/index.ts
+++ packages/client/src/ts/services/index.ts
@@ -1,18 +1,18 @@
 // TODO should be loaded from app in future
 import "../resources/services/BlogResourceService";
+import "../resources/services/CollaborativewallResourceService";
 import "../resources/services/MindmapResourceService";
 import "../resources/services/ScrapbookResourceService";
-import "../resources/services/CollaborativewallResourceService";
-import "../resources/services/WikiResourceService";
 
 import { IOdeServices, OdeServices } from "./OdeServices";
 
 export const odeServices: IOdeServices = new OdeServices().initialize();
 
-export type { ILinkedResource } from "../resources/behaviours/AbstractBehaviourService"; // FIXME to be removed when dropping behaviours
-export * from "../resources/SnipletsService"; // FIXME to be removed when dropping behaviours
 export * from "../directory/interface";
+export type { ILinkedResource } from "../resources/behaviours/AbstractBehaviourService"; // FIXME to be removed when dropping behaviours
 export * from "../resources/interface";
+export * from "../resources/ResourceService";
+export * from "../resources/SnipletsService"; // FIXME to be removed when dropping behaviours
 export * from "../rights/interface";
 export * from "../share/interface";
 export * from "../workspace/interface";
