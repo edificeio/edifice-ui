cd680350314ca9afad63b8f31a4e4e8551f188d9
Clement Creusat <clement.creusat@gmail.com>
2024-08-26 17:20:58 +0200

    feat: #WB2-1844, re-organize tiptap-extensions in one package

--- packages/tiptap/extensions/src/transform/index.ts
+++ packages/tiptap/extensions/src/transform/index.ts
@@ -1,4 +1,4 @@
-import htmlToJson from "./html-to-json/html-to-json";
-import jsonToHtml from "./json-to-html/json-to-html";
+import htmlToJson from './html-to-json/html-to-json';
+import jsonToHtml from './json-to-html/json-to-html';
 
 export { htmlToJson, jsonToHtml };
