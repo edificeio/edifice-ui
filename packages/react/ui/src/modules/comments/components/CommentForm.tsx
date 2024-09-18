8ec192ab59e1b63d58691c167e8eef27b254532e
Headan <36904370+Headan@users.noreply.github.com>
2024-09-18 17:22:54 +0200

    feat: #WB2-1761, add props type if comment provider is in read or in … (#237)

--- packages/react/ui/src/modules/comments/components/CommentForm.tsx
+++ packages/react/ui/src/modules/comments/components/CommentForm.tsx
@@ -8,44 +8,48 @@ import { TextCounter } from "./TextCounter";
 
 export const CommentForm = ({ userId }: { userId: string }) => {
   const { t } = useTranslation();
-  const { content, handleChangeContent, handleCreateComment, options } =
+  const { content, handleChangeContent, handleCreateComment, options, type } =
     useCommentsContext();
 
   const [ref, onFocus] = useAutosizeTextarea();
 
   return (
-    <div className="border rounded-3 p-12 pb-8 d-flex gap-12 bg-gray-200">
-      <CommentAvatar id={userId as string} />
-      <div className="d-flex flex-column flex-fill gap-4">
-        <textarea
-          id="add-comment"
-          ref={ref}
-          value={content}
-          className="form-control"
-          placeholder={t("comment.placeholder.textarea")}
-          maxLength={options.maxCommentLength as number}
-          onChange={handleChangeContent}
-          onFocus={onFocus}
-          rows={1}
-          style={{ resize: "none", overflow: "hidden" }}
-        />
-        <div className="d-flex justify-content-end align-items-center gap-4">
-          <TextCounter
-            content={content}
-            maxLength={options.maxCommentLength as number}
-          />
-          <Button
-            type="submit"
-            variant="ghost"
-            size="sm"
-            leftIcon={<Send />}
-            disabled={!content?.length}
-            onClick={() => handleCreateComment(content)}
-          >
-            {t("comment.post")}
-          </Button>
+    <>
+      {type === "edit" && (
+        <div className="border rounded-3 p-12 pb-8 d-flex gap-12 bg-gray-200">
+          <CommentAvatar id={userId as string} />
+          <div className="d-flex flex-column flex-fill gap-4">
+            <textarea
+              id="add-comment"
+              ref={ref}
+              value={content}
+              className="form-control"
+              placeholder={t("comment.placeholder.textarea")}
+              maxLength={options.maxCommentLength as number}
+              onChange={handleChangeContent}
+              onFocus={onFocus}
+              rows={1}
+              style={{ resize: "none", overflow: "hidden" }}
+            />
+            <div className="d-flex justify-content-end align-items-center gap-4">
+              <TextCounter
+                content={content}
+                maxLength={options.maxCommentLength as number}
+              />
+              <Button
+                type="submit"
+                variant="ghost"
+                size="sm"
+                leftIcon={<Send />}
+                disabled={!content?.length}
+                onClick={() => handleCreateComment(content)}
+              >
+                {t("comment.post")}
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
