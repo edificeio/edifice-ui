e5dc5c9dc364c83b9ef1d7dfb6c3e43c3dce12e3
RomuC <romuald.coulaud@gmail.com>
2024-09-24 10:37:12 +0200

    fix(editor): #WB-3226 remove editor image attribute textAlign (#245)

--- packages/react/editor/src/components/Renderer/MediaRenderer.tsx
+++ packages/react/editor/src/components/Renderer/MediaRenderer.tsx
@@ -77,7 +77,12 @@ const MediaRenderer = (props: MediaResizeProps) => {
         <div data-drag-handle>
           {node.type.name === "custom-image" ? (
             <Image
-              {...node.attrs}
+              src={node.attrs.src}
+              title={node.attrs.title}
+              alt={node.attrs.alt}
+              width={node.attrs.width}
+              style={node.attrs.style}
+              height={node.attrs.height}
               className={`custom-image`}
               ref={resizableMedia as React.RefObject<HTMLImageElement>}
             />
